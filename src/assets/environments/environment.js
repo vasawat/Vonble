0076c154c68408dc8f9e3ed2b7215d25205a76ff
vasawat prapachaimongkol <vasawat.ppcmk@gmail.com>
2024-10-21 11:15:45 +0700

    railway

--- src/assets/environments/environment.js
+++ src/assets/environments/environment.js
@@ -1,6 +1,7 @@
 const enviroment = {
-    apiUrl: "http://localhost:5000",
+    // apiUrl: "http://localhost:5000",
     // apiUrl: "https://vonble-backend.onrender.com",
+    apiUrl: "https://vonblebackend-production.up.railway.app",
 
 };
 
